87f85ba68862935f87f7e8f5802d5884ea6d4cb5
fredziak55 <jan.czyz100@gmail.com>
2026-05-11 23:26:38 +0200

    added working worker

--- src/controllers/scrape.controller.js
+++ src/controllers/scrape.controller.js
@@ -2,7 +2,9 @@ import scrapeIntercoolers from "../utils/scrape.js";
 
 export const scrape = (req, res) => {
     try {
-        const maxPages = req.body.maxPages = parseInt(req.body.maxPages) || 1;
+        const maxPages = parseInt(req.body.max_pages) || 1;
+        console.log(req.body);
+        console.log('Max pages to scrape:', maxPages);
         scrapeIntercoolers(maxPages);
         res.status(200).json({ message: 'Scraping started' });
     } catch (error) {
--- src/utils/scrape.js
+++ src/utils/scrape.js
@@ -2,14 +2,23 @@ import { getPage, getPageDetails } from "../models/scrape.model.js";
 import { purgeIntercoolers } from "../models/intercoolers.model.js";
 import { insertIntercoolers } from "../models/intercoolers.model.js";
 import { Queue } from 'bullmq';
+import IORedis from 'ioredis';
+
+const connection = new IORedis({
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: Number(process.env.REDIS_PORT || 6379),
+    maxRetriesPerRequest: null,
+});
 
 const scrapeIntercoolers = async (maxPages) => {
     await purgeIntercoolers();
-    const myQueue = new Queue('scrapePage');
+    const myQueue = new Queue('scrapePage', { connection });
 
     for (let page = 1; page <= maxPages; page++) {
         await myQueue.add('scrapePage', { page });
     }
+
+    await myQueue.close();
 }
 
 export default scrapeIntercoolers;
