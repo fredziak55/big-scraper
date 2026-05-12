d67263d0b0d41f8b657bcb64b349bfbd3b643b75
fredziak55 <jan.czyz100@gmail.com>
2026-05-12 23:32:16 +0200

    added more workers for docker scaling with async data read

--- src/models/database.model.js
+++ src/models/database.model.js
@@ -9,13 +9,16 @@ const __dirname = path.dirname(__filename)
 export const initializeDatabase = () => {
   const db = new sqlite3.Database(__dirname + '/../../db/cars.db')
   db.run('CREATE TABLE IF NOT EXISTS intercoolers (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price REAL, dimensions TEXT, url TEXT UNIQUE, capacityCm3 REAL, pricePerCm3 REAL)')
+  db.run('PRAGMA journal_mode=WAL;')
   db.close()
 }
 
 export const getDatabaseConnection = async () => {
-  return open({
+  const db = await open({
     filename: __dirname + '/../../db/cars.db',
     driver: sqlite3.Database
   })
+  await db.run('PRAGMA busy_timeout=5000;')
+  return db
 }
 
